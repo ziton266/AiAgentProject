9da0ee14acbb3823344f894694d0547c9bb96075
ziton266 <fareszitouni1999@gmail.com>
2025-02-25 22:06:52 +0100

    adding swagger configuration & Logging applicatif

---
+++ src/main/java/vermeg/springAI/AIAgent/controller/LoggingController.java
@@ -0,0 +1,23 @@
+package vermeg.springAI.AIAgent.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class LoggingController {
+
+    private static final Logger logger = LogManager.getLogger(LoggingController.class);
+    @GetMapping
+    public String testLogging() {
+        logger.info("Info: This is a test log message.");
+        logger.debug("Debug: This is a debug log message.");
+        logger.error("Error: This is an error log message.");
+        return "Check logs for output!";
+    }
+
+
+}
---
+++ src/test/java/vermeg/springAI/AIAgent/AiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package vermeg.springAI.AIAgent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
